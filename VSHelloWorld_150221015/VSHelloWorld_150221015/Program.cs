ed0e2ae621882fa251ecf76b0c69cbb10c9a8c0b
vasconicolau <32711134+vasconicolau@users.noreply.github.com>
2017-10-18 16:16:22 +0100

    comentário

---
+++ VSHelloWorld_150221015/VSHelloWorld_150221015/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace VSHelloWorld_150221015
+{
+    public class Program
+    {
+        //vasco comentário
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
